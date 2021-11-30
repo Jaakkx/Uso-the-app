3ff01878b8119d9bc103082916ab646e360e40bd
Jaakkx <78911141+Jaakkx@users.noreply.github.com>
2021-11-30 10:09:56 +0100

    Add button :radio_button:

---
+++ front/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export type ButtonProps = {
+  };
+
+class Button extends React.Component<ButtonProps>{
+
+    render() {
+        return(
+            <a target="_blank" href="">Le bouton</a>        
+        );
+    }
+
+}
+
+export default Button;
