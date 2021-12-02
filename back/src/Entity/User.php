2dc10e5b533f63b172340811c5d085001cdd9719
Alexis Kessab <alexis.kessab@orange.fr>
2021-12-02 09:39:21 +0100

    Osu & spotify api integration

--- back/src/Entity/User.php
+++ back/src/Entity/User.php
@@ -18,9 +18,10 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="text")
      */
     private $tokenSpotify;
+
 
     public function getId(): ?int
     {
--- back/src/Service/OsuService.php
+++ back/src/Service/OsuService.php
@@ -1,12 +1,8 @@
 <?php
-
 namespace App\Service;
-
-// use App\Entity\NotionPage;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-
 
 class OsuService
 {
@@ -25,7 +21,6 @@ class OsuService
 	 */
 	private $parameterBag;
 
-
 	public function __construct(
 		EntityManagerInterface $entityManager, 
 		HttpClientInterface $httpClient, 
@@ -35,6 +30,7 @@ class OsuService
 		$this->httpClient = $httpClient;
 		$this->parameterBag = $paramaterBag;
 	}
+
 	public function getOsuToken(): array
 	{
 		$osuBaseUrl = $this->parameterBag->get('osu_base_url');
@@ -42,8 +38,6 @@ class OsuService
 		// $osuMusicUrl = $this->parameterBag->get('osu_music_url');
 		$osuSecret = $this->parameterBag->get('osu_secret');
 		$osuClientId = $this->parameterBag->get('osu_client_id');
-
-		
 		$response = $this->httpClient->request('POST', $osuBaseUrl, [
 			'headers'=>['Accept' => 'application/json'],
 			'body'=>[
@@ -59,46 +53,52 @@ class OsuService
 		$osuUserPseudo = "yosh1ko";
 		$osuUserIdUrl = $osuUserUrl . $osuUserPseudo;
 		$responseUserId = $this->httpClient->request('GET', $osuUserIdUrl, [
-					'headers'=>[
-						'Accept' => 'application/json',
-						'Authorization' => $authorizationHeader,
-					],
-					'body'=>[
-						'client_id' => $osuClientId,
-						'client_secret' => $osuSecret,
-						'grant_type' => 'client_credentials',
-						'scope' => 'public',
-					],
-				]);
+			'headers'=>[
+				'Accept' => 'application/json',
+				'Authorization' => $authorizationHeader,
+			],
+			'body'=>[
+				'client_id' => $osuClientId,
+				'client_secret' => $osuSecret,
+				'grant_type' => 'client_credentials',
+				'scope' => 'public',
+			],
+		]);
 
-				// A DEFINIR EN FRONT
-				$userId = json_decode($responseUserId->getContent(), true)["id"];
-				$musicRenderType = "most_played";
-				$musicRenderLimit = 4;
-
-				$osuMusicSearchUrl = sprintf("https://osu.ppy.sh/api/v2/users/%s/beatmapsets/%s?limit=%s", $userId, $musicRenderType, $musicRenderLimit);
-				$responseBeatmaps = $this->httpClient->request('GET', $osuMusicSearchUrl, [
-					'headers'=>[
-						'Accept' => 'application/json',
-						'Authorization' => $authorizationHeader,
-					],
-					'body'=>[
-						'client_id' => $osuClientId,
-						'client_secret' => $osuSecret,
-						'grant_type' => 'client_credentials',
-						'scope' => 'public',
-					],
-				]);
+			// A DEFINIR EN FRONT
+			$userId = json_decode($responseUserId->getContent(), true)["id"];
+			$musicRenderType = "most_played";
+			$musicRenderLimit = 200;
+			$osuMusicSearchUrl = sprintf("https://osu.ppy.sh/api/v2/users/%s/beatmapsets/%s?limit=%s", $userId, $musicRenderType, $musicRenderLimit);
+			$responseBeatmaps = $this->httpClient->request('GET', $osuMusicSearchUrl, [
+				'headers'=>[
+					'Accept' => 'application/json',
+					'Authorization' => $authorizationHeader,
+				],
+				'body'=>[
+					'client_id' => $osuClientId,
+					'client_secret' => $osuSecret,
+					'grant_type' => 'client_credentials',
+					'scope' => 'public',
+				],
+			]);
 
 		$musicTitleTab = [];
+		$mmm = [];
 		$musicInfos = json_decode($responseBeatmaps->getContent(), true);
+		// return $musicInfos;
+		// return $responseBeatmaps;
 		foreach ($musicInfos as $key => $v){
 			$musicTitle = $musicInfos[$key]["beatmapset"]["title"];
 			$musicTitle = str_replace(" (TV Size)", "", $musicTitle);
+			$musicTitle = str_replace(" (TV edit)", "", $musicTitle);
+			$musicTitle = str_replace(" [TV Size]", "", $musicTitle);
 			$musicTitle = str_replace(" [Dictate Edit]", "", $musicTitle);
-			var_dump($musicTitle);
-			array_push($musicTitleTab, $musicTitle);
+			$musicId = $musicInfos[$key]["beatmap"]["beatmapset_id"];
+			// $musicId = $musicInfos[$key]["beatmap_id"];
+			array_push($musicTitleTab, ["id" =>$musicId, "titre" => $musicTitle]);
 		}
+		// return $musicInfos;
 		return $musicTitleTab;
 	}
 }
