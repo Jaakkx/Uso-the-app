a9d3c766013eccf2b65f9f9b1cd18d6bd9ae88fe
Jaakkx <78911141+Jaakkx@users.noreply.github.com>
2021-12-04 19:36:59 +0100

    Some advancement

--- front/src/components/PlaylistPage.tsx
+++ front/src/components/PlaylistPage.tsx
@@ -1,26 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { Route, Router } from "react-router-dom";
 import Banner from "./Banner";
-import OsuPage from "./OsuPage";
+import SearchOsu from "./SearchOsu";
 import SpotifyPage from "./SpotifyPage";
 
-function PlaylistPage (){
+interface AppContextInterface {
+    musicTab:any,
+}
 
-    const [token, setToken] = useState(0);
-    const [isStock, setStock] = useState(1);
+export const AppCtx = React.createContext<AppContextInterface | null>(null);
+const sampleAppContext: AppContextInterface = {
+    musicTab:["pull_data(tab)"],
+};
 
-    useEffect(() => {
+const PlaylistPage = () => {
+    const [tab, setTab] = useState([""]);
+    const [ok, setOk] = useState(true);
+console.log(ok);
 
-        console.log(localStorage.getItem('userToken'));
+    const pull_data = (data:any) => {
+        console.log(data);
+        
+        if(ok){
+            data['idTab'][0] = data["idTab"][1];
+            data['idTab'].splice(0,1);
+            setOk(false);
+        }
+        let interTab:Array<string> = [];
+        console.log(data['idTab']);
+        
+        for(let i=1;i<=data.length; i++){
+            interTab.push(data[i]);
+            // console.log("lol");
+            // console.log(interTab);
+        }
+        setTab(interTab);
+        return(interTab);
+    }
 
-  
-    })
-    
     return(
         <div className="App">
             <Banner />
-            <SpotifyPage />
-            <OsuPage />
+            <AppCtx.Provider value={sampleAppContext}>
+                <SpotifyPage UseValue={tab}/>
+            </AppCtx.Provider>
+            <SearchOsu onSuccess={pull_data}/>
         </div>
     )
 }
