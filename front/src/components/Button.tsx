e8f3a9cc042387af51ae233d84a7195bb81f6b76
Jaakkx <78911141+Jaakkx@users.noreply.github.com>
2021-12-05 13:28:39 +0100

    Change hyperlink

--- front/src/components/Button.tsx
+++ front/src/components/Button.tsx
@@ -7,7 +7,7 @@ class Button extends React.Component<ButtonProps>{
 
     render() {
         return(
-            <a target="_blank" href="https://accounts.spotify.com/authorize?client_id=29ced1155da2459f8e661f5beac00a74&response_type=code&redirect_uri=http://127.0.0.1:8081/&scope=user-read-private">Le bouton</a>
+            <a target="_blank" href="https://accounts.spotify.com/authorize?client_id=29ced1155da2459f8e661f5beac00a74&response_type=code&redirect_uri=http://uso-api.jael-beining.fr/&scope=user-read-private">Le bouton</a>
         );
     }
 
